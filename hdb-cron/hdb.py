3d300ac79d4c48adcb211bd427c077992149a480
limhawjia <hawjiaa@gmail.com>
2020-01-18 22:51:13 +0800

    Consolidate cron

--- hdb-cron/hdb.py
+++
@@ -1,8 +0,0 @@
-# Python program to pull, normalize and store HDB parking data.
-
-from datetime import datetime
-import pytz
-
-singapore_timezone = pytz.timezone('Asia/Singapore')
-current_datetime = datetime.now(singapore_timezone).strftime("%H:%M:%S")
-print("Hello world! It is now " + current_datetime)
