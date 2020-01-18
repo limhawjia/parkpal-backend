0fe10d796901943ca3f8db92566cae2056312b09
limhawjia <hawjiaa@gmail.com>
2020-01-18 15:58:06 +0800

    Create travis file and scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+scp -i ./credentials -o StrictHostKeyChecking=no -pr "$(pwd)" ubuntu@ec2-54-255-145-37.ap-southeast-1.compute.amazonaws.com:~/
+ssh -i ./credentials -o StrictHostKeyChecking=no ubuntu@ec2-54-255-145-37.ap-southeast-1.compute.amazonaws.com cd ParkPalBackend && docker-compose build
+ssh -i ./credentials -o StrictHostKeyChecking=no ubuntu@ec2-54-255-145-37.ap-southeast-1.compute.amazonaws.com docker swarm init || true
+ssh -i ./credentials -o StrictHostKeyChecking=no ubuntu@ec2-54-255-145-37.ap-southeast-1.compute.amazonaws.com docker stack deploy --compose-file ./ParkPalBackend/docker-compose.staging.yml ParkPal
+
+
