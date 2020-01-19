ab0533049b85565b5e476a42f73fa20aaa523105
Huang Weijie <h_weijie@yahoo.com>
2020-01-19 12:10:43 +0800

    Add some stuff

--- main/database/carpark.py
+++ main/database/carpark.py
@@ -1,5 +1,7 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, DECIMAL
+from sqlalchemy.ext.hybrid import hybrid_method
+import math
 
 
 class CarPark(Base):
@@ -10,3 +12,7 @@ class CarPark(Base):
     longitude = Column(DECIMAL, nullable=False)
     latitude = Column(DECIMAL, nullable=False)
     lots_available = Column(Integer)
+
+    @hybrid_method
+    def distance_from(self, latitude, longitude):
+        return math.sqrt(math.fabs(self.latitude - latitude) ** 2 + math.fabs(self.longitude - longitude) ** 2)
--- main/parkpal.py
+++ main/parkpal.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask import request
+from flask import abort
+from main.database import Database
+from main.database import CarPark
+import math
+import decimal
 
 app = Flask(__name__)
 
@@ -6,3 +12,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello world!!!'
+
+
+@app.route('/availability')
+def get_availability():
+    latitude = request.args.get('latitude', default=None, type=decimal) or abort(400, 'latitude not specified')
+    longitude = request.args.get('longitude', default=None, type=decimal) or abort(400, 'longitude not specified')
+    radius = request.args.get('radius', defaut=500, type=decimal)
+    session = Database().get_session()
+
+
+def query_carparks_within(x, y, radius_interval, session):
+    queries = session.query(CarPark) \
+        .order_by(CarPark.distance_from(x, y))
+    results = []
+    max_radius = radius_interval
+
+    for query in queries:
+        yield query
+
