b58dc4d5adf52beffe1e6c27e603ca29fca46d12
Huang Weijie <h_weijie@yahoo.com>
2020-01-19 19:06:06 +0800

    Implement distance from on entity

--- main/database/carpark.py
+++ main/database/carpark.py
@@ -1,6 +1,10 @@
 from .database import Base
 from .source import Source
-from sqlalchemy import Column, Integer, String, DECIMAL, Enum
+from sqlalchemy import Column, Integer, Float, String, DECIMAL, Enum, func
+from sqlalchemy.ext.hybrid import hybrid_method
+import math
+
+EARTH_RADIUS = 6378.137  # in km
 
 
 class CarPark(Base):
@@ -8,13 +12,27 @@ class CarPark(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     address = Column(String, nullable=False)
     price = Column(DECIMAL)
-    longitude = Column(DECIMAL, nullable=False)
-    latitude = Column(DECIMAL, nullable=False)
+    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=False)
     lots_available = Column(Integer)
     source = Column(Enum(Source, name="source"), nullable=False, )
     third_party_id = Column(String, nullable=False)
 
-
     @hybrid_method
     def distance_from(self, latitude, longitude):
-        return math.sqrt(math.fabs(self.latitude - latitude) ** 2 + math.fabs(self.longitude - longitude) ** 2)
+        lat_rad_diff = math.radians(latitude - self.latitude)
+        lon_rad_diff = math.radians(longitude - self.longitude)
+        a = math.sin(lat_rad_diff / 2) ** 2 + \
+            math.cos(math.radians(self.latitude)) * math.cos(math.radians(latitude)) * \
+            math.sin(lon_rad_diff / 2) ** 2
+
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        d = EARTH_RADIUS * c
+
+        return d * 1000  # in meters
+
+    @distance_from.expression
+    def distance_from(cls, latitude, longitude, app):
+        a = func.power(func.abs(cls.latitude - latitude), 2)
+        b = func.power(func.abs(cls.longitude - longitude), 2)
+        return func.sqrt(a + b)
--- main/parkpal.py
+++ main/parkpal.py
@@ -3,7 +3,8 @@ from flask import request
 from flask import abort
 from main.database import Database
 from main.database import CarPark
-import decimal
+from decimal import Decimal
+import math
 
 app = Flask(__name__)
 
@@ -11,20 +12,36 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello world!!!'
-#
-#
-# @app.route('/availability')
-# def get_availability():
-#     latitude = request.args.get('latitude', default=None, type=decimal) or abort(400, 'latitude not specified')
-#     longitude = request.args.get('longitude', default=None, type=decimal) or abort(400, 'longitude not specified')
-#     radius = request.args.get('radius', defaut=100.0, type=decimal)
-#     session = Database().get_session()
-#     query_carparks_within(latitude, longitude)
-#
-#
-# def query_carparks_within(latitude, longitude, radius, session):
-#     queries = session.query(CarPark) \
-#         .order_by(CarPark.distance_from(latitude, longitude))
-#
-#     for query in queries:
-#         print(query)
+
+
+@app.route('/availability')
+def get_availability():
+    latitude = request.args.get('latitude', default=None, type=float) or abort(400, 'latitude not specified')
+    longitude = request.args.get('longitude', default=None, type=float) or abort(400, 'longitude not specified')
+    # radius = request.args.get('radius', default=100.0, type=Decimal)
+    # session = Database().get_session()
+    # query_carparks_within(latitude, longitude, radius, session)
+    f = distance_from(0.0, 0.0, latitude, longitude)
+    app.logger.warning(str(f))
+    return str(f)
+
+
+def query_carparks_within(latitude, longitude, radius, session):
+    queries = session.query(CarPark) \
+        .order_by(CarPark.distance_from(latitude, longitude, app))
+
+    for query in queries:
+        app.logger.warning("query is %s", query)
+
+
+def distance_from(latitude1, longitude1, latitude2, longitude2):
+    lat_rad_diff = math.radians(latitude2 - latitude1)
+    lon_rad_diff = math.radians(longitude2 - longitude1)
+    a = math.sin(lat_rad_diff / 2) ** 2 + \
+        math.cos(math.radians(latitude1)) * math.cos(math.radians(latitude2)) * \
+        math.sin(lon_rad_diff / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = 6378.137 * c
+
+    return d * 1000  # in meters
+
