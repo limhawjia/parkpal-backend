fd04c20afc57e4f24b03eb13375f8f7d5bdb0e4f
limhawjia <hawjiaa@gmail.com>
2020-01-19 18:49:02 +0800

    Finish hdb cron program

--- main/database/carpark_utils.py
+++ main/database/carpark_utils.py
@@ -21,3 +21,27 @@ def update_carpark_metadata(carpark_data_models):
         query.longitude = carpark.longitude
         query.latitude = carpark.latitude
         session.commit()
+
+    session.close()
+
+
+def update_carpark_availability(data_sets):
+    session = Database.get_instance().get_session()
+
+    for data in data_sets:
+        source = data[0]
+        third_party_id = data[1]
+        lots_available = data[2]
+
+        query = session.query(CarPark) \
+            .filter(CarPark.source == source) \
+            .filter(CarPark.third_party_id == third_party_id) \
+            .first()
+
+        if not query:
+            continue
+
+        query.lots_available = lots_available
+        session.commit()
+
+    session.close()
---
+++ main/hdb/hdb_cron.py
@@ -0,0 +1,47 @@
+import itertools
+from datetime import datetime
+
+import pytz
+import requests
+
+from main.database.source import Source
+
+import main.database.carpark_utils as cu
+
+url = 'https://api.data.gov.sg/v1/transport/carpark-availability'
+
+
+def log_pull():
+    singapore_timezone = pytz.timezone('Asia/Singapore')
+    current_datetime = datetime.now(singapore_timezone).strftime("%H:%M:%S")
+    print("Pulling carpark availability from HDB: " + current_datetime)
+
+
+def pull():
+    response = requests.get(url)
+
+    if not response.content:
+        print("Empty response. HDB carpark metadata not available.")
+        return
+
+    raw_data = response.json()["items"]["carpark_data"]
+
+    transformations1 = itertools.islice(raw_data, 10)
+    transformations2 = map(convert_to_data_set, transformations1)
+    carpark_data_sets = list(transformations2)
+
+    cu.update_carpark_availability(data_sets=carpark_data_sets)
+
+
+def convert_to_data_set(raw_data):
+    source = Source.HDB
+    third_party_id = raw_data["carpark_number"]
+    lots_available = raw_data["carpark_info"]["lots_available"]
+
+    return source, third_party_id, lots_available
+
+
+def start():
+    log_pull()
+    pull()
+    print("Pull succeeded.")
