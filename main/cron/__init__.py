fa99e6024670af1649b33ec9492c8136d79f0277
limhawjia <hawjiaa@gmail.com>
2020-01-21 19:59:51 +0800

    Make query limit and cron frequency varibales optional

--- main/cron/__init__.py
+++ main/cron/__init__.py
@@ -1,3 +1,8 @@
 import os
+import sys
 
-MetadataQueryLimit = int(os.environ.get('QUERY_LIMIT', None) or exit('QUERY_LIMIT environment variable not defined.'))
+MetadataQueryLimit = int(os.environ.get('QUERY_LIMIT', None))
+
+if MetadataQueryLimit is None:
+    MetadataQueryLimit = sys.maxsize
+    print(f"QUERY_LIMIT not set, defaulting to max size: {MetadataQueryLimit}")
--- scripts/cron_start.sh
+++ scripts/cron_start.sh
@@ -2,4 +2,12 @@
 
 printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
 python -m main.startup
+
+if [ -z "$CRON_FREQUENCY" ]
+then
+  echo "CRON_FREQUENCY environment variable not set, defaulting to 1 minute."
+else
+  sed -i "s/\* \* \* \* \*/\*\/$CRON_FREQUENCY \* \* \* \*/g" ./cron
+fi
+
 cron -f
